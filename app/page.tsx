c2153ac36ced421a0f793508f1a93c6db9e61f53
Chao Zhou <Czhou0k@gmail.com>
2023-06-14 09:51:36 -0400

    removed global css import and comment out thinbackend until further notice

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { initThinBackend } from 'thin-backend';
 import { ThinBackend } from 'thin-backend-react';
+
+// initThinBackend({ host: process.env.NEXT_PUBLIC_BACKEND_URL });
 
 export default function Home() {
   return (
